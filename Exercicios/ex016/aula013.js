c0254357b95393daa77eaa14c28f9143d2f0d0c2
PAULO CESAR NORMANDIA <115199995+pcnormandia@users.noreply.github.com>
2022-12-07 08:46:44 -0300

    Create aula013.js

---
+++ Exercicios/ex016/aula013.js
@@ -0,0 +1,22 @@
+/* Programação sequencial
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+
+/*Programação com teste lógico no inicio (enquanto...faça)
+var c = 1
+while(c<=6){
+    console.log('Tudo bem?')
+    c++
+}*/
+
+//Programação com teste lógico no final (faça...enquanto)
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+}while(c<=6)
