8e06cbb3620b4b376ae00bf8e6c983f89ea792cc
PAULO CESAR NORMANDIA <115199995+pcnormandia@users.noreply.github.com>
2022-12-09 09:34:34 -0300

    Update script.js

--- Exercicios/ex021/script.js
+++ Exercicios/ex021/script.js
@@ -3,30 +3,46 @@ let lista = document.querySelector('select#flista') //Criar variável para receb
 let res = document.querySelector('div#res') //Criar uma váriavel para mostrar os resultados finais
 let valores = [] //Criar uma variável do tipo array para receber os valores digitados
 
-function isNumero(n){//Função para validação dos dados: para verificar se o número está entre 1 e 100 e vazia
+//Função para validação dos dados: para verificar se o número está entre 1 e 100 e vazia
+function isNumero(n){
     if(Number(n) >=1 && Number(n) <= 100){
         return true
     }else{
         return false
     }
 }
-function inLista(n, l){//Função para validação dos dados: não podem repetir no array
+//Função para validação dos dados: verificar que os dados não estão repetidos
+function inLista(n, l){
     if(l.indexOf(Number(n)) !=-1){
         return true
     }else{
         return false
     }
 }
-
-function adicionar(){//Adicionar os valores validos no array
+//Adicionar os valores validos no array
+function adicionar(n){
+    //Desvio condicional para validação dos dados
     if(isNumero(num.value) && !inLista(num.value, valores)){
-        window.alert('Tudo OK!')
+        valores.push(Number(num.value))
+        lista.innerHTML = `O valor ${Number(num.value)} foi inserido na lista`
     }else{
         window.alert('Valor inválido ou já encontrado na lista')
-    }    
+    }
+    
 }
+
+function finalizar(){
+    for(let c in valores){
+        if(c==0){
+            let maior = valores[0]
+        }else if(valores[c]>maior){
+            maior = valores[c]
+        }
+    }
+    res.innerHTML = `O maior valor obtido foi ${maior}`
+}
+
 //limpar o campo dos resultados finais
-//Validação de dados: verificar se input está vazia    
 
 //Mostrar os valores do array em um elemento select
 //Limpar os dados de análise dos números
@@ -35,7 +51,6 @@ function adicionar(){//Adicionar os valores validos no array
 
 //Função para analisar os números obtidos na array
 
-//Validação dos dados: mostrar msg alerta caso não haja nenhum valor no array (length==0)
 //Contar os valores válidos cadastrados no array
 //Verificar o maior valor válido cadastrado
 //Verificar o menor valor válido cadastrado
